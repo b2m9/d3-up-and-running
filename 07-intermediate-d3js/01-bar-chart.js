ae5f1d597a99e032174eb8e55b2ad5ef40a260e5
b2m9 <bob@b2m9.com>
2016-10-17 22:52:19 +0200

    Beautify me
    - Add some styles
    - Add colours and colours for high values
    - Add tooltip

--- 07-intermediate-d3js/01-bar-chart.js
+++ 07-intermediate-d3js/01-bar-chart.js
@@ -7,11 +7,11 @@
   // Dimensions
   var dim = {
     w: 700,
-    h: 350
+    h: 400
   }
   var offset = {
     left: 50,
-    top: 20,
+    top: 50,
     right: 20,
     bottom: 20
   }
@@ -42,18 +42,41 @@
     bars.enter().append('rect')
       .attr('class', 'bar')
       .attr('x', function (d, i) { return x(i) })
-      .attr('y', function (d) { return y(d) })
-      .attr('height', function (d) { return h - y(d) })
       .attr('width', x.bandwidth())
+      .attr('y', h)
+      .attr('height', 0)
+      .on('mouseover', function (d, i) {
+        hoverIn(this, d)
+      })
+      .on('mouseout', hoverOut)
+      .merge(bars)
+        .transition()
+          .duration(750)
+          .delay(function (d, i) { return 25 * i })
+          .attr('y', function (d) { return y(d) })
+          .attr('height', function (d) { return h - y(d) })
+          .on('end', function () {
+            d3.select(this)
+              .classed('bar--high', function (d) { return (d >= 20) })
+          })
   }
 
   function plotAxes (ctx, x, y) {
     var bottomAxis = d3.axisBottom().scale(x).tickFormat('')
-      .tickSizeInner(4)
+      .tickSizeInner(4).tickSizeOuter(0)
     var leftAxis = d3.axisLeft().scale(y)
 
-    ctx.select('.x.axis').call(bottomAxis)
-    ctx.select('.y.axis').call(leftAxis)
+    ctx.select('.x.axis')
+      .attr('opacity', 0)
+      .call(bottomAxis)
+        .transition().duration(750).delay(500)
+        .attr('opacity', 1)
+
+    ctx.select('.y.axis')
+      .attr('opacity', 0)
+      .call(leftAxis)
+        .transition().duration(750).delay(500)
+        .attr('opacity', 1)
   }
 
   function calcXScale (data, dim, offset) {
@@ -66,6 +89,26 @@
   function calcYScale (data, dim, offset) {
     return d3.scaleLinear().domain([0, d3.max(data)])
       .range([dim.h - offset.top - offset.bottom, 0])
+  }
+
+  function hoverIn (ctx, val) {
+    var dimBar = ctx.getBoundingClientRect()
+
+    d3.select('#value').text(val)
+
+    var dimTip = document.getElementById('tooltip').getBoundingClientRect()
+
+    d3.select('#tooltip')
+      .style('left', (dimBar.left - dimTip.width / 4) + 'px')
+      .style('top', (dimBar.top - dimTip.height - 10) + 'px')
+      .transition().duration(250)
+        .style('opacity', 1)
+  }
+
+  function hoverOut () {
+    d3.select('#tooltip')
+      .transition().duration(250)
+        .style('opacity', 0)
   }
 
   function init (dim, offset) {
